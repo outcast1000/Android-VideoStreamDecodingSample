576b7ede0baf523e694d24b2cd29de3441daba00
outcast1000 <a.economou@gmail.com>
2021-04-28 16:35:33 +0300

    Keep Logcat logs in the internal memory for debug later. (It will be placed in /data/data/com.dji.videostreamdecodingsample/files/logs/ directory)

--- android-videostreamdecodingsample/app/src/main/java/com/dji/videostreamdecodingsample/VideoDecodingApplication.java
+++ android-videostreamdecodingsample/app/src/main/java/com/dji/videostreamdecodingsample/VideoDecodingApplication.java
@@ -2,6 +2,11 @@ package com.dji.videostreamdecodingsample;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Environment;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 import dji.sdk.base.BaseProduct;
 import dji.sdk.sdkmanager.DJISDKManager;
@@ -25,5 +30,59 @@ public class VideoDecodingApplication extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         com.secneo.sdk.Helper.install(VideoDecodingApplication.this);
+    }
+
+
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_LONG);
+        if ( isExternalStorageWritable() ) {
+
+            File logDirectory = new File( getFilesDir() + "/logs" );
+            File logFile = new File( logDirectory, "logcat_" + System.currentTimeMillis() + ".txt" );
+            Toast.makeText(getApplicationContext(), logFile.toString(), Toast.LENGTH_LONG);
+
+            // create app folder
+            if ( !logDirectory.exists() ) {
+                logDirectory.mkdir();
+            }
+
+            // create log folder
+            if ( !logDirectory.exists() ) {
+                logDirectory.mkdir();
+            }
+
+            // clear the previous logcat and then write the new one to the file
+            try {
+                Process process = Runtime.getRuntime().exec("logcat -c");
+                process = Runtime.getRuntime().exec("logcat -f " + logFile);
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+
+        } else if ( isExternalStorageReadable() ) {
+            // only readable
+        } else {
+            // not accessible
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if ( Environment.MEDIA_MOUNTED.equals( state ) ) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if ( Environment.MEDIA_MOUNTED.equals( state ) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals( state ) ) {
+            return true;
+        }
+        return false;
     }
 }
